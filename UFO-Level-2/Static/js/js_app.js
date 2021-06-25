4e266a362237f7c50e5f77499eea8722e9d5613e
Tom Garrity <tomgarrity2020@gmail.com>
2021-06-25 15:45:37 -0400

    add tbale data

--- UFO-Level-2/Static/js/js_app.js
+++ UFO-Level-2/Static/js/js_app.js
@@ -2,3 +2,18 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// Using the UFO dataset provided, append a table to a web page and then adds new rows of data for each UFO sighting.
+var tbody = d3.select("tbody");
+var option_list = [];
+
+tableData.forEach(ufodata => {
+  var row = tbody.append("tr");
+  Object.entries(ufodata).forEach(function([key, value]) {
+    var cell = row.append("td");
+    cell.text(value);
+    option_list.push("");
+    option_list.push(ufodata.shape);
+  });
+});
+
